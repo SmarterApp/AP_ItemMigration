fe8284c94769d42bcf2f9f27486c2b75d8c09d91
Brett Drainer <brett@gearlaunch.com>
2018-03-05 20:51:54 -0800

    Commented out the Kubernetes scripts.  We don't want a release build to deploy the migration tool because it will run it which we don't want.

--- src/main/kubernetes/k8s-redeploy.sh
+++ src/main/kubernetes/k8s-redeploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-kops export kubecfg --state @kubecfgState@ --name @kubecfgName@
+#kops export kubecfg --state @kubecfgState@ --name @kubecfgName@
 
 #Trigger rolling updates
-export DATE_PATCH="{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
+#export DATE_PATCH="{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
 
-kubectl patch deployment @kubecfgDeployment@ -p "$DATE_PATCH"
+#kubectl patch deployment @kubecfgDeployment@ -p "$DATE_PATCH"
--- src/main/kubernetes/k8s-update-deployment.sh
+++ src/main/kubernetes/k8s-update-deployment.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-kops export kubecfg --state @kubecfgState@ --name @kubecfgName@
-
-kubectl set image deployment/ap-item-migration-deployment ap-item-migration=@dockerTagBase@/ap-item-migration:@version@
+#kops export kubecfg --state @kubecfgState@ --name @kubecfgName@
+#
+#kubectl set image deployment/ap-item-migration-deployment ap-item-migration=@dockerTagBase@/ap-item-migration:@version@
